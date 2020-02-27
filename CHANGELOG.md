8a7c101b2566561a35f5f010b18d4661e58ba071
ROVEDA <zeh.henrique92@gmail.com>
2020-02-27 19:47:29 -0300

    Package docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 ## [1.0.0] - 2020-02-27.
 
 * This package allows to knows how is the network connection status, for Android and iOS, 'true' if is connected, 'false' if is not. Added example and README.
+
+## [1.0.1] - 2020-02-27.
+
+* This package allows to knows how is the network connection status, for Android and iOS, 'true' if is connected, 'false' if is not. Added example and README.
--- lib/connection_verify.dart
+++ lib/connection_verify.dart
@@ -1,19 +1,24 @@
 library connection_verify;
+
 import 'dart:io';
 import 'dart:async';
 
 class ConnectionVerify {
-
+  // The response depends on an asynchronous result
   static Future<bool> connectionStatus() async {
+    // Here, the result works like a 'ping' at the address below
     try {
+      // It's used 'example.com' because it has a global range
       final result = await InternetAddress.lookup('example.com');
       if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
         print('connected');
+        // Here it's returned 'true', so the response of 'lookup' was sucessful
         return true;
       }
       return false;
     } on SocketException catch (_) {
       print('not connected');
+      // Here the 'lookup' returned and exception, so there is not connection
       return false;
     }
   }
